42a5dc13082e067b3abc8f4a75a348c666ff3686
Simanga Shabalala <simangae@gmail.com>
2022-01-31 10:42:37 +0200

    ProductController and views

---
+++ A-StateOnline.UI/Controllers/ProductController.cs
@@ -0,0 +1,109 @@
+﻿using A_StateOnline.Core.Models;
+using A_StateOnline.DataAccess.Inmemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace A_StateOnline.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        ProductRepository context;
+        public ProductController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product prod = context.Find(Id);
+            if (prod == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+                prod.Category = product.Category;
+                prod.Description = product.Description;
+                prod.Image = product.Image;
+                prod.Name = product.Name;
+                prod.Price = product.Price;
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            Product prodiuctToDelete = context.Find(Id);
+            if (prodiuctToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(prodiuctToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product prodiuctToDelete = context.Find(Id);
+            if (prodiuctToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
