9f99c0f5a15598448c5a41db2848b6d75a5e2213
Simanga Shabalala <simangae@gmail.com>
2022-02-16 18:32:03 +0200

    Product Filtering

---
+++ A-StateOnline.Core/ViewModels/ProductListVM.cs
@@ -0,0 +1,15 @@
+﻿using A_StateOnline.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_StateOnline.Core.ViewModels
+{
+    public class ProductListVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
